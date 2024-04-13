36065c526c7c8454730c025672f227b013784cee
gexxii <gexielking31@gmail.com>
2024-04-12 20:06:51 -0400

    2dn commit

--- lib/Screens/Defensa Civil/noticias.dart
+++ lib/Screens/Defensa Civil/noticias.dart
@@ -11,9 +11,48 @@ class _NoticiasScreenState extends State<NoticiasScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color.fromRGBO(238, 120, 46, 1),
       body: Center(
-        child: Text('NoticiasScreen'),
-      )
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              height: 70,
+              width: 200,
+              decoration: BoxDecoration(
+                boxShadow: [
+                  BoxShadow(color: Colors.black, spreadRadius: 2, blurRadius: 10, blurStyle: BlurStyle.solid),
+                ],
+                borderRadius: BorderRadius.circular(20),
+                color: Colors.white,
+                border: Border.all(
+                  color: Colors.black,
+                  width: 2,
+                ),
+              ),
+              margin: EdgeInsets.only(bottom: 20),
+              child: Center(
+                child: Text(
+                  "Noticias",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 30,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              height: 450,
+              width: 300,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                color: Colors.white,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
